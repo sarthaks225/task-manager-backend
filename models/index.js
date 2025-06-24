c2d0b583156c0b241111a9cd3512b661bb7235ef
Sarthak Sharma <sarthaks225@gmail.com>
2025-06-24 14:03:28 +0530

    sequelize index.js changes

--- models/index.js
+++ models/index.js
@@ -8,7 +8,14 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
     dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // Render uses self-signed certificates
+      },
+    },
   }
 );
 

fb989e83890b0a9f7e1634298619837e9b1dff20
Sarthak Sharma <sarthaks225@gmail.com>
2025-06-25 01:00:27 +0530

    changes in authController  login error

--- controllers/authController.js
+++ controllers/authController.js
@@ -19,15 +19,25 @@ exports.signup = async (req, res) => {
 
 exports.login = async (req, res) => {
   try {
+    console.log("Login request body:", req.body);
     const { email, password } = req.body;
     const user = await User.findOne({ where: { email } });
     if (!user) return res.status(404).json({ message: "User not found" });
 
     const match = await bcrypt.compare(password, user.password);
-    if (!match) return res.status(401).json({ message: "Invalid credentials" });
+    if (!match) return res.status(400).json({ message: "Invalid credentials" });
 
     const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
-    res.json({ token });
+
+    res.json({
+      token,
+      user: {
+        firstName: user.firstName,
+        lastName: user.lastName,
+        emailt: user.email,
+        fullName: user.fullName,
+      },
+    });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
